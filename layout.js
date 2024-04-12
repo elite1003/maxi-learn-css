d1ded989c48ea0a5e65e7fa24c23aab8bd76d2b9
elite1003 <sudhirkumar10397@gmail.com>
2024-04-12 22:33:39 +0530

    add customers, packages page. add modal, backdrop. add layout.js and layout.css

---
+++ layout.js
@@ -0,0 +1,29 @@
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const planButtons = document.querySelectorAll(".plan button");
+const modalActionNo = document.querySelector(".modal__action--negative");
+const mobileNav = document.querySelector(".mobile-nav");
+const toggleButton = document.querySelector(".toggle-button");
+const openModal = () => {
+  modal.classList.add("open");
+  backdrop.classList.add("open");
+};
+const closeModal = () => {
+  if (modal) modal.classList.remove("open");
+  backdrop.classList.remove("open");
+};
+for (const planButton of planButtons) {
+  planButton.addEventListener("click", openModal);
+}
+backdrop.addEventListener("click", () => {
+  closeModal();
+  mobileNav.classList.remove("open");
+});
+
+toggleButton.addEventListener("click", () => {
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
+if (modalActionNo) {
+  modalActionNo.addEventListener("click", closeModal);
+}
